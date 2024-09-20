ec2c49a13abc09815455872c8f83a2e782ae06e5
Lucie Gernidos <lgernido@made-f0Dr11s5.clusters.42paris.fr>
2024-09-20 10:48:50 +0200

    Docker ready to be used + database connexion working

--- backend/transcendence/settings.py
+++ backend/transcendence/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(ezrh!_r8=w4ksnuzvty@@^n$6w%4kn_^!0v8h&kw5yq15=-&r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+PORT = 8000
 
 
 # Application definition
--- db/init.sql
+++ db/init.sql
@@ -1,11 +1,1 @@
 CREATE DATABASE superpongdb;
-DO
-$$
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'superpong') THEN
-        CREATE ROLE superpong LOGIN PASSWORD 'gnoprepus';
-    END IF;
-END
-$$;
-
-GRANT ALL PRIVILEGES ON DATABASE superpongdb TO superpong;
