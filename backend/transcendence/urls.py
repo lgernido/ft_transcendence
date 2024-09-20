4f77058245d3a201f19618cc6c941b76c2a015f5
Lucie Gernidos <lgernido@made-f0Dr11s5.clusters.42paris.fr>
2024-09-20 11:40:35 +0200

    added the user app for database design purposes

--- backend/transcendence/urls.py
+++ backend/transcendence/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('user/', include("user.urls")),
     path('game/', include("game.urls")),
     path('admin/', admin.site.urls),
 ]
