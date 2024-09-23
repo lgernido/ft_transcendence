1e6e799ba76991676e8b91ebd4c596e6670e8012
Corentin Nguyen <cornguye@made-f0Cr7s3.clusters.42paris.fr>
2024-09-23 11:59:44 +0200

    fix lauch docker-compose

--- db/init.sql
+++ db/init.sql
@@ -1,1 +1,8 @@
-CREATE DATABASE superpongdb;
+-- Check if the database exists
+\l superpongdb
+
+
+-- If the database doesn't exist, create it
+\if ! \echo '\l | grep -q superpongdb'
+    CREATE DATABASE superpongdb;
+\endif
