42026293a2f7ba5cef49ad669520b3f35425cac0
Samuel Cely <scely@bess-f2r1s9.clusters.42paris.fr>
2024-11-12 21:02:30 +0100

    change: quelque modif

--- backend/src/chat/admin.py
+++ backend/src/chat/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Channel, Message
 
 # Register your models here.
+admin.site.register(Channel)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('channel', 'content', 'timestamp') 
--- backend/src/chat/views.py
+++ backend/src/chat/views.py
@@ -1,19 +1,28 @@
-# views.py
 from django.shortcuts import get_object_or_404, render
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model
-from .models import Channel
+from .models import Channel, Message
 import os
 
 User = get_user_model()
+
+# Vue pour charger les anciens messages
+def load_messages(request, channel_id):
+    channel = get_object_or_404(Channel, id=channel_id)
+    messages = Message.objects.filter(channel=channel).order_by('timestamp')
+    messages_data = [
+        {'sender': msg.sender.username, 'content': msg.content, 'timestamp': msg.timestamp.isoformat()}
+        for msg in messages
+    ]
+    return JsonResponse({'messages': messages_data})
+
 def index(request):
     return render(request, 'chat/chat-room.html')
 
-# Dans le cas ou cela ne fonctionne pas regarder si les routes sont ou urls sont bonne
 def search_users(request):
     query = request.GET.get('query', '')
     if len(query) >= 2:
-        users = User.objects.filter(username__icontains=query)
+        users = User.objects.filter(username__icontains=query, is_staff=False)
         results = [{'username': user.username, 'id': user.id} for user in users]
         return JsonResponse({'results': results})
     return JsonResponse({'results': []})
@@ -25,7 +34,7 @@ def create_private_channel(request, user_id):
     # Vérifier si le canal privé existe déjà
     channel, created = Channel.objects.get_or_create(
         mode=1,  # Mode 'Private message'
-        users__in=[user],
+        users__in=[user, other_user],
     )
     # Si le canal vient d'être créé, ajouter les utilisateurs
     if created:
