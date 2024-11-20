b09d58b19db26950eab0c49e792514fc168b1938
Samuel Cely <scely@bess-f4r2s7.clusters.42paris.fr>
2024-11-20 09:47:50 +0100

    update: Mise a jour du dockerfile du backend et .yml

---
+++ backend/conf/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Script de démarrage pour Django
+
+echo "Appliquer les migrations..."
+python src/manage.py migrate #--noinput
+python src/manage.py makemigrations #--noinput
+
+echo "Créer un super utilisateur..."
+python src/manage.py shell -c "
+from django.contrib.auth import get_user_model;
+import os;
+User = get_user_model();
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin');
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com');
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'password123');
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password);
+"
+
+
+echo "Lancer le serveur Django..."
+exec "$@"
