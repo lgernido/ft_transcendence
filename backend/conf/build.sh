0fa2dbb9c2842820ff190a35520f2c8577ea248c
Samuel Cely <scely@bess-f4r2s7.clusters.42paris.fr>
2024-11-21 13:23:48 +0100

    update .env

--- backend/conf/build.sh
+++ backend/conf/build.sh
@@ -10,9 +10,9 @@ python src/manage.py shell -c "
 from django.contrib.auth import get_user_model;
 import os;
 User = get_user_model();
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin');
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com');
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'password123');
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME');
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL');
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD');
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password);
 "
