e69113edb4cb361599149f651716d24b25c2114d
Samuel Cely <scely@bess-f2r1s11.clusters.42paris.fr>
2024-11-28 15:13:13 +0100

    fixe: some problems

--- backend/src/transcendence/settings.py
+++ backend/src/transcendence/settings.py
@@ -17,7 +17,6 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
  
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -49,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
 ]
 
 MIDDLEWARE = [
