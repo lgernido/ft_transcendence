ba5126c83fd5f01bd9ffc2f237dedd7aceda8a00
Lucie Gernidos <luciegernidos.pro@gmail.com>
2024-12-02 15:18:09 +0100

    Clean translation - Big format change in amis.html and amis.js - Upper buttons don't work

--- backend/src/game/static/js/language-switch.js
+++ backend/src/game/static/js/language-switch.js
@@ -1,5 +1,8 @@
+let currentLanguage = localStorage.getItem('language') || 'en';
+
 function selectLanguage() {
     document.querySelector('.language-switcher form').addEventListener('submit', function(event) {
         window.location.reload();
     });
 }
+
